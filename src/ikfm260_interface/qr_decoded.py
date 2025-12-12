a8b7fa9dae92164ea6413bc7c85d2f677615ce37
lesguillemets <kgkacko+github@gmail.com>
2025-12-12 19:22:23 +0900

    gradually move to lazy api

--- src/ikfm260_interface/qr_decoded.py
+++ src/ikfm260_interface/qr_decoded.py
@@ -1,39 +1,43 @@
 import polars as pl
 from pathlib import Path
 
-from ikfm260_interface.base import add_file_info
+from ikfm260_interface.base import gen_file_info_expr
 from ikfm260_interface.consts import CONDITION1_MAP, CONDITION2_MAP, EMOTION_MAP
 
 
-def load_qr_data(file_path: Path) -> pl.DataFrame:
+QR_SCHEMA_OVERRIDES = {
+    "x": pl.Int8,
+    "y": pl.Int8,
+    "condition1": pl.Int8,
+    "condition2": pl.Int8,
+    "layout": pl.Int8,
+    "emotion": pl.Int8,
+    "procedure": pl.Int8,
+}
+
+
+def load_qr_data(file_path: Path) -> pl.LazyFrame:
     """
     Reads facemimic TSV data nicely
     file_path: Path to the TSV file
     """
 
-    df = pl.read_csv(
+    df = pl.scan_csv(
         file_path,
         separator=",",
-        schema_overrides={
-            "x": pl.Int8,
-            "y": pl.Int8,
-            "condition1": pl.Int8,
-            "condition2": pl.Int8,
-            "layout": pl.Int8,
-            "emotion": pl.Int8,
-            "procedure": pl.Int8,
-        },
+        schema_overrides=QR_SCHEMA_OVERRIDES,
         has_header=True,
     )
 
     # Add some helpful transformations
-    df = add_file_info(df, file_path, "qr").with_columns(
+    df = df.with_columns(
         [
             # Create meaningful condition labels using dictionary mapping
             pl.col("condition1").replace_strict(CONDITION1_MAP).alias("VisualFeedback"),
             pl.col("condition2").replace_strict(CONDITION2_MAP).alias("Reference"),
             pl.col("emotion").replace_strict(EMOTION_MAP).alias("emotion_str"),
         ]
+        + gen_file_info_expr(file_path, "qr")
     )
 
     return df
