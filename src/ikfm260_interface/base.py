b0cef926ed574519541842fde2baee4eb72c0007
lesguillemets <kgkacko+github@gmail.com>
2025-12-12 19:18:31 +0900

    prefer using list of expressions

--- src/ikfm260_interface/base.py
+++ src/ikfm260_interface/base.py
@@ -4,13 +4,15 @@ import polars as pl
 from ikfm260_interface.filehandle.base import get_id_from_filename
 
 
-def add_file_info(df: pl.DataFrame, source_file: Path, file_kind) -> pl.DataFrame:
+def gen_file_info_expr(source_file: Path, file_kind: str) -> list[pl.Expr]:
     participant_id = get_id_from_filename(source_file)
-    return df.with_columns(
-        [
-            # participant_id is created from file name
-            pl.lit(participant_id).alias("participant_id"),
-            # add which file the data came from
-            pl.lit(source_file.name).alias(f"{file_kind}_file_name"),
-        ]
-    )
+    return [
+        # participant_id is created from file name
+        pl.lit(participant_id).alias("participant_id"),
+        # add which file the data came from
+        pl.lit(source_file.name).alias(f"{file_kind}_file_name"),
+    ]
+
+
+def add_file_info(df: pl.DataFrame, source_file: Path, file_kind: str) -> pl.DataFrame:
+    return df.with_columns(gen_file_info_expr(source_file, file_kind))
